7efc31346b21107f5236853e01838f1eb7577e8c
jeonghean-kim <jeonghean.kim@gmail.com>
2020-06-19 15:15:13 +0900

    add sider yml

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
+  console.log('Github');
   return (
     <>
       <StatusBar barStyle="dark-content" />
